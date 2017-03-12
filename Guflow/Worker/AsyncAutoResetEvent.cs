e83962fadb3f2d6e535b69bdafe975e07c6da2e2
gurmitteotia <gurmit.teotia@gmail.com>
2017-03-12 19:59:13 +0000

    Fixed failing tests in ActivityExecution

---
+++ Guflow/Worker/AsyncAutoResetEvent.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Guflow.Worker
+{
+    public class AsyncAutoResetEvent
+    {
+        private static readonly Task Completed = Task.FromResult(true);
+        private readonly Queue<TaskCompletionSource<bool>> _waits = new Queue<TaskCompletionSource<bool>>();
+        private bool _signaled;
+
+        public Task WaitAsync()
+        {
+            lock (_waits)
+            {
+                if (_signaled)
+                {
+                    _signaled = false;
+                    return Completed;
+                }
+                else
+                {
+                    var tcs = new TaskCompletionSource<bool>();
+                    _waits.Enqueue(tcs);
+                    return tcs.Task;
+                }
+            }
+        }
+
+        public void Set()
+        {
+            TaskCompletionSource<bool> toRelease = null;
+
+            lock (_waits)
+            {
+                if (_waits.Count > 0)
+                    toRelease = _waits.Dequeue();
+                else if (!_signaled)
+                    _signaled = true;
+            }
+            if (toRelease != null)
+                toRelease.SetResult(true);
+        }
+    }
+}
