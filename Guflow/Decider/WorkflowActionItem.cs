6a03f39502ef2bf2e5b2975417e671a5bae6ef90
gurmitteotia <gurmit.teotia@gmail.com>
2017-04-13 09:04:55 +0100

    starting support for adding schedulable workflow actionW @

--- Guflow/Decider/WorkflowActionItem.cs
+++ Guflow/Decider/WorkflowActionItem.cs
@@ -1,10 +1,12 @@
-﻿namespace Guflow.Decider
+﻿using System.Collections.Generic;
+
+namespace Guflow.Decider
 {
-    internal class WorkflowActionItem :  IFluentWorkflowActionItem
+    internal class WorkflowActionItem : WorkflowItem, IFluentWorkflowActionItem
     {
         private readonly WorkflowAction _workflowAction;
 
-        public WorkflowActionItem(WorkflowAction workflowAction)
+        public WorkflowActionItem(WorkflowAction workflowAction): base(Identity.New(""),)
         {
             _workflowAction = workflowAction;
         }
@@ -15,6 +17,46 @@
         }
 
         public IFluentWorkflowActionItem After(string activityName, string activityVersion, string positionalName = "")
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override WorkflowItemEvent LastEvent
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override IEnumerable<WorkflowItemEvent> AllEvents
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        internal override WorkflowDecision GetScheduleDecision()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal override WorkflowDecision GetCancelDecision()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal override WorkflowAction TimerFired(TimerFiredEvent timerFiredEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal override WorkflowAction TimerCancelled(TimerCancelledEvent timerCancelledEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal override WorkflowAction TimerStartFailed(TimerStartFailedEvent timerStartFailedEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal override WorkflowAction TimerCancellationFailed(TimerCancellationFailedEvent timerCancellationFailedEvent)
         {
             throw new System.NotImplementedException();
         }
