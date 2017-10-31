722943632f58e2ee613e4e34772f6f75d4406b3d
Gurmit Singh <gurmit.singh@cloudhouse.com>
2017-10-31 19:04:52 +0000

    Updated to latest packages and updated readme file

--- Guflow.Tests/Properties/AssemblyInfo.cs
+++ Guflow.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Guflow.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit tests for guflow")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Guflow.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Gurmit Singh Teotia, Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
