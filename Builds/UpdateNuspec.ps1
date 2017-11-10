c222035bc0c1fc759b1b3e8f6b77c1c73eab71fa
Gurmit Teotia <gurmit.teotia@gmail.com>
2017-11-10 10:03:13 +0000

    Added powershell script to update nuspec file

---
+++ Builds/UpdateNuspec.ps1
@@ -0,0 +1,25 @@
+$checkoutPath = (Get-Item Env:APPVEYOR_BUILD_FOLDER).Value
+
+$nuspecFilePath = Join-Path $checkoutPath -ChildPath "Guflow\Guflow.nuspec"
+$packageFilePath = Join-Path $checkoutPath -ChildPath "Guflow\packages.config"
+
+$nuspecContent = [xml](Get-Content $nuspecFilePath )
+$packageContent = [xml](Get-Content $packageFilePath)
+
+$nuspecNamespace = $nuspecContent.package.xmlns
+#update version
+$nuspecContent.version = (Get-Item Env:APPVEYOR_BUILD_VERSION).Value
+
+#update copyright version
+$nuspecContent.copyright = "Copyright "+[char]0x00A9 + " "+(Get-Date).year + " - Gurmit Teotia"
+
+#Update dependency in nuspec file
+foreach($package in $packageContent.packages.package)
+{
+    $dependency = $nuspecContent.CreateElement("dependency", $nuspecNamespace)
+    $dependency.SetAttribute("id",$package.id)
+    $dependency.SetAttribute("version", $package.version)
+    $nuspecContent.package.metadata.dependencies.AppendChild($dependency)
+}
+
+$nuspecContent.Save($nuspecFilePath)
