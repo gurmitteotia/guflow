40939b8bee0f9152e54fe5f5a995f60a6bcbfb6f
Gurmit Teotia <gurmit.teotia@gmail.com>
2018-09-21 11:18:44 +0100

    I-19 Added Timer.IsFired and Timer.IsCancelled extension method

--- Guflow.Tests/Decider/Timer/TimerItemsExtensionTests.cs
+++ Guflow.Tests/Decider/Timer/TimerItemsExtensionTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Gurmit Teotia. Please see the LICENSE file in the project root for license information.
 using System;
 using System.Collections.Generic;
+using Amazon.SimpleWorkflow.Model;
 using Guflow.Decider;
 using Moq;
 using NUnit.Framework;
@@ -10,6 +11,15 @@ namespace Guflow.Tests.Decider
     [TestFixture]
     public class TimerItemsExtensionTests
     {
+        private const string Timer1 = "name1";
+        private EventGraphBuilder _eventGraphBuilder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _eventGraphBuilder = new EventGraphBuilder();
+        }
+
         [Test]
         public void First_test()
         {
@@ -25,9 +35,52 @@ namespace Guflow.Tests.Decider
 
             Assert.Throws<ArgumentNullException>(() => timerItems.First(""));
         }
+
+        [Test]
+        public void IsFired_when_last_event_is_fired()
+        {
+            var completedGraph = _eventGraphBuilder.TimerFiredGraph(Identity.Timer(Timer1), TimeSpan.Zero);
+            var timer = CreateTimerItemFor(completedGraph);
+
+            Assert.IsTrue(timer.IsFired());
+        }
+        [Test]
+        public void IsFired_when_last_event_is_started()
+        {
+            var completedGraph = _eventGraphBuilder.TimerStartedGraph(Identity.Timer(Timer1), TimeSpan.Zero);
+            var timer = CreateTimerItemFor(completedGraph);
+
+            Assert.IsFalse(timer.IsFired());
+        }
+
+        [Test]
+        public void IsCancelled_when_last_event_is_cancelled()
+        {
+            var completedGraph = _eventGraphBuilder.TimerCancelledGraph(Identity.Timer(Timer1), TimeSpan.Zero);
+            var timer = CreateTimerItemFor(completedGraph);
+
+            Assert.IsTrue(timer.IsCancelled());
+        }
+        [Test]
+        public void IsCancelled_when_last_event_is_started()
+        {
+            var completedGraph = _eventGraphBuilder.TimerStartedGraph(Identity.Timer(Timer1), TimeSpan.Zero);
+            var timer = CreateTimerItemFor(completedGraph);
+
+            Assert.IsFalse(timer.IsCancelled());
+        }
+
         private static ITimerItem CreateTimer(string name)
         {
             return TimerItem.New(Identity.Timer(name), Mock.Of<IWorkflow>());
+        }
+
+        private TimerItem CreateTimerItemFor(IEnumerable<HistoryEvent> eventGraph)
+        {
+            var workflowHistoryEvents = new WorkflowHistoryEvents(eventGraph);
+            var workflow = new Mock<IWorkflow>();
+            workflow.SetupGet(w => w.WorkflowHistoryEvents).Returns(workflowHistoryEvents);
+            return TimerItem.New(Identity.Timer(Timer1), workflow.Object);
         }
     }
 }
--- Guflow/Decider/Timer/TimerItemsExtension.cs
+++ Guflow/Decider/Timer/TimerItemsExtension.cs
@@ -6,10 +6,31 @@ namespace Guflow.Decider
 {
     public static class TimerItemsExtension
     {
+        /// <summary>
+        /// Returns the first scheduled timer object for the given name.
+        /// </summary>
+        /// <param name="timerItems"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public static ITimerItem First(this IEnumerable<ITimerItem> timerItems, string name)
         {
             Ensure.NotNull(timerItems, "timerItems");
             return timerItems.OfType<TimerItem>().First(t => t.Has(Identity.Timer(name)));
         }
+
+        /// <summary>
+        /// Returns true if last event of timer is fired event.
+        /// </summary>
+        /// <param name="timerItem"></param>
+        /// <returns></returns>
+        public static bool IsFired(this ITimerItem timerItem) => timerItem.LastEvent() is TimerFiredEvent;
+
+        /// <summary>
+        /// Returns true if last of event of timer is cancelled event.
+        /// </summary>
+        /// <param name="timerItem"></param>
+        /// <returns></returns>
+        public static bool IsCancelled(this ITimerItem timerItem) => timerItem.LastEvent() is TimerCancelledEvent;
+
     }
 }
